b88b864e79462fca9cf6d9e6d9f4f2ea6faef803
Gabriel Minton <g.m.minton@wustl.edu>
2024-11-22 23:28:05 -0600

    Split values

--- Exploratory.r
+++ Exploratory.r
@@ -1,1 +1,16 @@
-data <- data(iris)
+# Load packages
+library(dplyr)
+
+# Load data
+data(iris)
+summary(iris)
+
+# Split dataset into different species
+#setosa, virginica, versicolor
+names(iris) <- tolower(names(iris))
+setosa <- filter(iris, species == 'setosa')
+virginica <- filter(iris, species == 'virginica')
+versicolor <- filter(iris, species == 'versicolor')
+
+plot(setosa)
+plot(setosa$petal.length, setosa$petal.width)
